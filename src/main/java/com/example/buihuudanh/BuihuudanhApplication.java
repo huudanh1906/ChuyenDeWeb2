dd060d6ac98a9ef7dfdffd088aab0446056b5424
unknown <doanhbui.5207@gmail.com>
2025-02-24 10:42:12 +0700

    first commit

---
+++ src/main/java/com/example/buihuudanh/BuihuudanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.buihuudanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuihuudanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuihuudanhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buihuudanh/BuihuudanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buihuudanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuihuudanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
